95b06f2e895c06529d9315651ecd3a5bd1fca40d
Eli Fine <ejfine@gmail.com>
2024-12-16 19:15:47 +0000

    using yaml for CI tests

--- extensions/context.py
+++ extensions/context.py
@@ -24,6 +24,7 @@ class ContextUpdater(ContextHook):
         context["gha_setup_python"] = "v5.3.0"
         context["gha_cache"] = "v4.2.0"
         context["gha_upload_artifact"] = "v4.4.3"
+        context["gha_mutex"] = "d3d5b354d460d4b6a1e3ee5b7951678658327812 # v1.0.0-alpha.9"
         context["gha_linux_runner"] = "ubuntu-24.04"
         #######
         return context
