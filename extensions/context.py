4d36e7c852316009f10676eb67898d0693382872
Eli Fine <eli88fine@gmail.com>
2025-01-24 11:05:39 -0500

    Basic ability to have templates focused on NodeJS and Pulumi (#10)
    ## Why is this change necessary?
    Need to do some frontend work and Infrastructure
    
    
     ## How does this change address the issue?
    Adds some gitignore statements and updates some files to exclude in
    pre-commit config
    
    Adds some pulumi packages to Context, and a question in the template
    about using Pulumi
    
    
     ## What side effects does this change have?
    None
    
    
     ## How is this change tested?
    aws-organizations repo and nuxt-static template repo.
    
    
     ## Other
    Bumps some precommit hooks and uv
    
    DRYs up code by using the `manual-install-deps.sh` script it more
    places. Needed to add argument to script to just install and not check
    the lock file (for use in copier template CI)
    
    ---------
    
    Co-authored-by: Eli Fine <ejfine@gmail.com>

--- extensions/context.py
+++ extensions/context.py
@@ -11,7 +11,7 @@ class ContextUpdater(ContextHook):
     @override
     def hook(self, context: dict[Any, Any]) -> dict[Any, Any]:
         # These are duplicated in the install-ci-tooling.sh script in this repository
-        context["uv_version"] = "0.5.16"
+        context["uv_version"] = "0.5.21"
         context["pre_commit_version"] = "4.0.1"
         # These also in pyproject.toml
         context["copier_version"] = "9.4.1"
@@ -24,11 +24,17 @@ class ContextUpdater(ContextHook):
         context["pytest_cov_version"] = "6.0.0"
         #######
         context["sphinx_version"] = "8.1.3"
+        context["pulumi_version"] = "3.147.0"
+        context["pulumi_aws_version"] = "6.66.3"
+        context["pulumi_aws_native_version"] = "1.24.0"
+        context["pulumi_command_version"] = "1.0.1"
+        context["boto3_version"] = "1.36.3"
         # These are duplicated in the CI files for this repository
         context["gha_checkout"] = "v4.2.2"
         context["gha_setup_python"] = "v5.3.0"
         context["gha_cache"] = "v4.2.0"
         context["gha_upload_artifact"] = "v4.4.3"
+        context["gha_configure_aws_credentials"] = "v4.0.2"
         context["gha_mutex"] = "d3d5b354d460d4b6a1e3ee5b7951678658327812 # v1.0.0-alpha.9"
         context["gha_linux_runner"] = "ubuntu-24.04"
         # These also in the tests/data.yml files in this repository and in copier.yaml
