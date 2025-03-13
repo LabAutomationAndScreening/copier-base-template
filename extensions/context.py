a8cbebad028aad5a7d4072f9b2a562528d949fce
Eli Fine <eli88fine@gmail.com>
2025-03-13 09:16:34 -0400

    Bump some versions (#12)
    Bump some misc dependency versions

--- extensions/context.py
+++ extensions/context.py
@@ -11,7 +11,7 @@ class ContextUpdater(ContextHook):
     @override
     def hook(self, context: dict[Any, Any]) -> dict[Any, Any]:
         # These are duplicated in the install-ci-tooling.sh script in this repository
-        context["uv_version"] = "0.6.3"
+        context["uv_version"] = "0.6.6"
         context["pre_commit_version"] = "4.1.0"
         # These also in pyproject.toml
         context["copier_version"] = "9.5.0"
@@ -24,25 +24,26 @@ class ContextUpdater(ContextHook):
         context["pytest_cov_version"] = "6.0.0"
         #######
         context["sphinx_version"] = "8.1.3"
-        context["pulumi_version"] = "3.153.1"
+        context["pulumi_version"] = "3.155.0"
         context["pulumi_aws_version"] = "6.67.0"
         context["pulumi_aws_native_version"] = "1.25.0"
         context["pulumi_command_version"] = "1.0.1"
-        context["boto3_version"] = "1.36.3"
+        context["pulumi_github"] = "6.7.0"
+        context["boto3_version"] = "1.37.11"
         context["ephemeral_pulumi_deploy_version"] = "0.0.2"
         context["pydantic_version"] = "2.10.6"
         context["pyinstaller_version"] = "6.12.0"
         # These are duplicated in the CI files for this repository
         context["gha_checkout"] = "v4.2.2"
-        context["gha_setup_python"] = "v5.3.0"
+        context["gha_setup_python"] = "v5.4.0"
         context["gha_cache"] = "v4.2.0"
         context["gha_upload_artifact"] = "v4.4.3"
         context["gha_configure_aws_credentials"] = "v4.0.2"
-        context["gha_mutex"] = "d3d5b354d460d4b6a1e3ee5b7951678658327812 # v1.0.0-alpha.9"
+        context["gha_mutex"] = "1ebad517141198e08d47cf72f3c0975316620a65 # v1.0.0-alpha.10"
         context["gha_linux_runner"] = "ubuntu-24.04"
         # These also in the tests/data.yml files in this repository and in copier.yaml
         context["py312_version"] = "3.12.7"  # ReadTheDocs does not yet support 3.12.8
-        context["py313_version"] = "3.13.1"
+        context["py313_version"] = "3.13.2"
         #######
         context["gha_windows_runner"] = "windows-2022"
         # Kludge to allow for the same docker-compose file in child and grandchild templates
