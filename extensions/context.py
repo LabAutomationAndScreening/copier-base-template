4e9a892f9d610717a4394f28ca5f41833f1fcd24
Eli Fine <eli88fine@gmail.com>
2025-03-18 18:05:38 +0000

    some python versions

--- extensions/context.py
+++ extensions/context.py
@@ -35,6 +35,9 @@ class ContextUpdater(ContextHook):
         context["pydantic_version"] = "2.10.6"
         context["pyinstaller_version"] = "6.12.0"
         context["setuptools_version"] = "76.0.0"
+        context["strawberry_graphql_version"] = "0.262.5"
+        context["fastapi_version"] = "0.115.11"
+        context["uvicorn_version"] = "0.34.0"
         # These are duplicated in the CI files for this repository
         context["gha_checkout"] = "v4.2.2"
         context["gha_setup_python"] = "v5.4.0"
