add722a43a447dd707bb731e4e33656e49c76b0d
Eli Fine <eli88fine@gmail.com>
2025-05-05 10:28:46 +0000

    fix port syntax

--- .devcontainer/install-ci-tooling.sh
+++ .devcontainer/install-ci-tooling.sh
@@ -6,13 +6,14 @@ curl -LsSf https://astral.sh/uv/0.6.17/install.sh | sh
 uv --version
 # TODO: add uv autocompletion to the shell https://docs.astral.sh/uv/getting-started/installation/#shell-autocompletion
 
-# Ensure that uv won't use the default system Python
-default_version="3.12.7"
+# Set to the system version of Python3 by default
+default_version=$(python3 -c "import sys; print ('.'.join((str(x) for x in sys.version_info[:3])))")
 
 # Use the input argument if provided, otherwise use the default value
 input="${1:-$default_version}"
 
 export UV_PYTHON="$input"
+echo "Using Python version: $UV_PYTHON"
 export UV_PYTHON_PREFERENCE=only-system
 
 uv tool install 'copier==9.6.0' --with 'copier-templates-extensions==0.3.0'
