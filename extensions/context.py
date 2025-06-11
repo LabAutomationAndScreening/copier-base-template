5c3b6351758c737fc29858abc572dc3da88d291f
Eli Fine <eli88fine@gmail.com>
2025-06-11 10:56:18 +0000

    nginx

--- extensions/context.py
+++ extensions/context.py
@@ -67,7 +67,8 @@ class ContextUpdater(ContextHook):
         context["gha_windows_runner"] = "windows-2025"
         #######
         context["debian_release_name"] = "bookworm"
-        context["alpine_image_version"] = "3.20"
+        context["alpine_image_version"] = "3.21"
+        context["nginx_image_version"] = "1.28.0"
         # These also in the tests/data.yml files in this repository and in copier.yaml
         context["py312_version"] = "3.12.7"  # ReadTheDocs does not yet support 3.12.8
         context["py313_version"] = "3.13.2"
