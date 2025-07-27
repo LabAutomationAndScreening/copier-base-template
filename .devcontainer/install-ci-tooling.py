07abf8fdff0fb3f866314de6937c39f5f9f6b916
Eli Fine <eli88fine@gmail.com>
2025-07-27 07:31:02 -0400

    Readme update (#72)
    ## Link to Issue or Message thread
    
    https://github.com/LabAutomationAndScreening/copier-base-template/issues/71
    
    
     ## Why is this change necessary?
    Missing a step in readme about staging the changes
    
    
     ## How does this change address the issue?
    Adds it
    
    
     ## What side effects does this change have?
    None
    
    
     ## How is this change tested?
    Downstream child and grandchild repos
    
    
     ## Other
    Bumps various versions, including a change in the name of
    copier-template-extensions

--- .devcontainer/install-ci-tooling.py
+++ .devcontainer/install-ci-tooling.py
@@ -5,9 +5,9 @@ import shutil
 import subprocess
 import sys
 
-UV_VERSION = "0.7.12"
-COPIER_VERSION = "9.7.1"
-COPIER_TEMPLATES_EXTENSION_VERSION = "0.3.1"
+UV_VERSION = "0.8.3"
+COPIER_VERSION = "9.8.0"
+COPIER_TEMPLATE_EXTENSIONS_VERSION = "0.3.2"
 PRE_COMMIT_VERSION = "4.2.0"
 GITHUB_WINDOWS_RUNNER_BIN_PATH = r"C:\Users\runneradmin\.local\bin"
 parser = argparse.ArgumentParser(description="Install CI tooling for the repo")
@@ -65,7 +65,7 @@ def main():
                 "install",
                 f"copier=={COPIER_VERSION}",
                 "--with",
-                f"copier-templates-extensions=={COPIER_TEMPLATES_EXTENSION_VERSION}",
+                f"copier-template-extensions=={COPIER_TEMPLATE_EXTENSIONS_VERSION}",
             ],
             check=True,
             env=uv_env,
