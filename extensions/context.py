7b5975a67eda3106574d7d1068f65c0f9f9b0009
Eli Fine <eli88fine@gmail.com>
2025-11-26 11:42:16 +0000

    nuxtui

--- extensions/context.py
+++ extensions/context.py
@@ -36,7 +36,7 @@ class ContextUpdater(ContextHook):
         context["pydantic_version"] = "2.12.4"
         context["pyinstaller_version"] = "6.16.0"
         context["setuptools_version"] = "80.7.1"
-        context["strawberry_graphql_version"] = "0.284.1"
+        context["strawberry_graphql_version"] = "0.287.0"
         context["fastapi_version"] = "0.121.0"
         context["fastapi_offline_version"] = "1.7.4"
         context["uvicorn_version"] = "0.38.0"
@@ -50,7 +50,7 @@ class ContextUpdater(ContextHook):
         context["python_kiota_bundle_version"] = "1.9.7"
         #######
         context["node_version"] = "24.7.0"
-        context["nuxt_ui_version"] = "^4.1.0"
+        context["nuxt_ui_version"] = "^4.2.1"
         context["nuxt_version"] = "^4.2.0"
         context["nuxt_icon_version"] = "^2.1.0"
         context["typescript_version"] = "^5.9.3"
