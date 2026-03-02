2bfd368a711dfa535ca10b0973570ebe9d81f9ec
Eli Fine <eli88fine@gmail.com>
2026-03-02 12:08:27 +0000

    more dev

--- .devcontainer/post-start-command.sh
+++ .devcontainer/post-start-command.sh
@@ -4,7 +4,7 @@ set -ex
 git config --global --add safe.directory /workspaces/copier-base-template
 pre-commit run merge-claude-settings -a
 
-if ! bd ready --json; then
+if ! bd ready; then
 	echo "It's likely the Dolt server has not yet been initialized to support beads, running that now" # TODO: figure out a better way to match this specific scenario than just a non-zero exit code...but beads still seems like in high flux right now so not sure what to tie it to
     # the 'stealth' flag is just the only way I could figure out how to stop it from modifying AGENTS.md...if there's another way to avoid that, then fine.  Even without the stealth flag though, files inside the .claude/beads directory get modified, so restoring them at the end to what was set in git...these shouldn't really need to change regularly
     # trying to set 'prefix' to nothing doesn't seem to work (it just acts like the prefix flag wasn't there), so just setting to 'work' as an arbitrary name
