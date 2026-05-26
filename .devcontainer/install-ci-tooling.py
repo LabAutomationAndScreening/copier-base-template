b29da6842342d705ce4d26266d7d1f757687c198
Eli Fine <eli88fine@gmail.com>
2026-05-26 10:18:03 +0000

    uv

--- .devcontainer/install-ci-tooling.py
+++ .devcontainer/install-ci-tooling.py
@@ -5,8 +5,8 @@ import shutil
 import subprocess
 import sys
 
-UV_VERSION = "0.11.14"
-PNPM_VERSION = "11.2.2"
+UV_VERSION = "0.11.16"
+PNPM_VERSION = "11.3.0"
 COPIER_VERSION = "9.15.1"
 COPIER_TEMPLATE_EXTENSIONS_VERSION = "0.3.3"
 PRE_COMMIT_VERSION = "4.5.1"
