9b35f11ef09c13fdddc960b66ffc645eb3a0986c
Eli Fine <eli88fine@gmail.com>
2026-06-29 19:52:06 +0000

    bump kiota

--- extensions/context.py
+++ extensions/context.py
@@ -132,7 +132,7 @@ class ContextUpdater(ContextHook):
         context["alpine_image_version"] = "3.23"
         context["nginx_image_version"] = "1.30.1"
         #######
-        context["kiota_cli_version"] = "1.31.4"
+        context["kiota_cli_version"] = "1.32.4"
         # These also in the tests/data.yml files in this repository and in copier.yaml
         context["py312_version"] = "3.12.7"  # ReadTheDocs does not yet support 3.12.8
         context["py313_version"] = "3.13.9"
